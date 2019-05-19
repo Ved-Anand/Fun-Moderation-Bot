70fa08c0bedaa823d6f24531dcfa47bc830eef5e
Phatfox2 <ved.anand@outlook.com>
2019-05-19 10:12:18 -0700

    Fixed reload command

--- commands/owner/reload.js
+++ commands/owner/reload.js
@@ -12,11 +12,10 @@ module.exports = {
         if (commandName === "reload") return message.channel.send("No reloading the reload command silly!");
         if(!args[1]) return message.channel.send("Please specify the directory the command is in!");
         let directory = args[1].toLowerCase();
-        if (directory != "owner" && directory != "other" && directory != "moderating") return message.channel.send("That directory was not found!");
         try {
             delete require.cache[require.resolve(`../${directory}/${commandName}.js`)] // usage !reload <name>
             bot.commands.delete(commandName);
-            const pull = require(`./${commandName}.js`);
+            const pull = require(`../${directory}/${commandName}.js`);
             bot.commands.set(commandName, pull);
             return message.channel.send(`The command \`${args[0].toUpperCase()}\` has been reloaded!`);
         } catch(e) {
