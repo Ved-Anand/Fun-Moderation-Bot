48859e6b7ae6ec7c81ab32791000cb7931668933
Phatfox2 <ved.anand@outlook.com>
2019-05-28 15:01:04 -0700

    Updates & Fixes

--- commands/other/calc.js
+++ commands/other/calc.js
@@ -1,4 +1,5 @@
 const { inspect } = require("util");
+const { ownerid } = require("../../tokenfile.json");
 var Fraction = require("fractional").Fraction;
 
 module.exports = {
@@ -11,6 +12,7 @@ module.exports = {
     },
     run: async (bot, message, args) => {
         let cmd = message.content.split(" ")[0];
+        if (message.author.id != ownerid) return message.channel.send("No.");
         if (args[0] == "help") return message.channel.send(`Usage: ${cmd} (mathematical expression) \n Example: ${cmd} 2 + 2  \n Example 2: ${cmd} round 7/8 + 2/8`);
         if (!args[0]) return message.channel.send("Mathematical Expression not found, type $calc help for more info on this.");
         let evaled;
@@ -27,17 +29,9 @@ module.exports = {
                 message.channel.send(inspect(frac));
             } else {
                 evaled = await eval(args.join(' '));
-                if (evaled % 1 != 0) {
-                    let frac = new Fraction(evaled);
-                    frac = frac.toString();
                     message.channel.send(inspect(evaled));
-                    message.channel.send("/");
-                    message.channel.send(inspect(frac));
-                } else {
-                    message.channel.send(inspect(evaled));
-                }
             }
-        } catch {
+        } catch(err) {
             message.reply("An error occurred, are you using the command right? type $calc help to find the command syntax.");
         }
         function round(num, places) {
--- commands/other/urban.js
+++ commands/other/urban.js
@@ -12,7 +12,7 @@ module.exports = {
         var targetWord = args == "" ? urban.random() : urban(args);
         var checkCody = message.content.split(" ")[1];
         if (args[0].toLowerCase() == "ved") return message.channel.send("Lol no");
-        if (checkCody == "cody") return message.channel.send("Cody: The most highly annoying person you'll ever meet. His name after all, literally means annoying. He is a pure menace. Whenever you find a cody, be sure to stay away from them.");
+        if (checkCody.toLowerCase() == "cody") return message.channel.send("Cody: The most highly annoying person you'll ever meet. His name after all, literally means annoying. He is a pure menace. Whenever you find a cody, be sure to stay away from them.");
         if (message.author.id == 460609573300994048) return message.channel.send("Lol no kedar xD");
         let cmd = message.content.split(" ")[0];
         if (args[0].toLowerCase() == "help") return usage.detailedHelp('other/urban', message.channel, 'Search something up on the urban dictionary', '')
