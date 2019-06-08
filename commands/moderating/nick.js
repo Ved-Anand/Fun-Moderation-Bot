ab349bba100955a7f49f8831f24e4b6623a45cf8
Phatfox2 <ved.anand@outlook.com>
2019-06-07 18:13:47 -0700

    New File

---
+++ commands/moderating/nick.js
@@ -0,0 +1,28 @@
+const errors = require("../../utils/errors.js");
+module.exports = {
+    config: {
+        name: "nick",
+        usage: "$nick <user> <nick-name>",
+        description: "Change someone's nickname",
+        permissions: "change nicknames",
+        aliases: ["nickname"]
+    },
+    run: async (bot, message, args) => {
+        if (message.channel.type == "dm") return;
+        if (!message.member.hasPermission("MANAGE_NICKNAMES")) return errors.noPerms(message, "CHANGE_NICKNAMES");
+        if (!message.guild.me.hasPermission("MANAGE_NICKNAMES")) return errors.lack(message.channel, "CHANGE_NICKNAMES");
+        let nUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if (!nUser) return errors.cantfindUser(message.channel);
+        let past = nUser.user.username;
+        if (nUser.id == bot.user.id) return errors.botuser(message, "nick");
+        if (nUser.hasPermission("ADMINISTRATOR")) return errors.equalPerms(message, nUser, "ADMINISTRATOR");
+        let nickname = args.join(" ").slice(22);
+        if (!nickname) return message.channel.send("No nickname was given!");
+        try {
+            message.guild.members.get(nUser.id).setNickname(nickname);
+            message.channel.send(`Changed ${past}'s name to ${nickname} successfully.`);
+        } catch {
+            message.channel.send("Unfortunately, an error occurred.")
+        }
+    }
+}
