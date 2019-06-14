b6aab854bb860c037747ee412ee54cce79c58723
Phatfox2 <ved.anand@outlook.com>
2019-06-13 18:18:32 -0700

    get rid of 1 collection

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("./error");
 const { Client, Collection } = require("discord.js"); //gets Collection + Client from Discord
 const bot = new Client(); //creates the Discord Client
 const yt = require("./loaders/reader");
-["aliases", "commands", "getal"].forEach(x => bot[x] = new Collection());       //for aliases + commands create new Collection
+["aliases", "commands"].forEach(x => bot[x] = new Collection());       //for aliases + commands create new Collection
 ["console", "command", "event"].forEach(x => require(`../handlers/${x}`)(bot))    //for console, command, event, for each require run function with bot parameter
 console.log("Successfully loaded other files.");
 bot.login(yt.token);
